09df95d669ee16d682098b140a59e7e18e2af0e6
Espeniv <espeiv@stud.ntnu.no>
2025-07-22 15:48:22 +0200

    Small fix to notification message trunctation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,6 +17,6 @@ export const formatRelativeTime = (timestamp: string) => {
 
 //To end notifications with a quotation mark after ...
 export function truncateWithQuote(str: string, maxLength: number) {
-  if (str.length <= maxLength) return str;
+  if (str.length <= maxLength) return str + '"';
   return str.slice(0, maxLength - 1) + '…"';
 }
