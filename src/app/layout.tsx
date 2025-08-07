0a530fb289603317923d18bc6e3f2288e8045a16
Espeniv <espeiv@stud.ntnu.no>
2025-08-07 15:14:24 +0200

    Reverted back to old emojis as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,7 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Eks",
   icons: {
-    icon: "/favicon.ico",
-    apple: "/apple-icon.png",
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🔶</text></svg>",
   },
 };
 
