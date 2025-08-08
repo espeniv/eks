a3adca299a098bae0f01dd353530d427adcda322
Espeniv <espeiv@stud.ntnu.no>
2025-08-08 14:55:21 +0200

    Removed animations on mobile navbar clicks

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,7 +14,7 @@ export function Navbar() {
     <nav className="fixed bottom-0 left-0 right-0 bg-black border-t border-gray-800 flex justify-around items-center py-2 z-50 md:hidden select-none">
       <Link
         href="/home"
-        className="flex flex-col items-center px-4 py-1 hover:text-orange-400 rounded-full active:scale-90 transition"
+        className="flex flex-col items-center px-4 py-1 hover:text-orange-400 rounded-full"
       >
         {pathname === "/home" || pathname.startsWith("/post/") ? (
           <svg
@@ -49,7 +49,7 @@ export function Navbar() {
       </Link>
       <Link
         href="/notifications"
-        className="flex flex-col items-center px-4 py-1 hover:text-orange-400 relative rounded-full active:scale-90 transition"
+        className="flex flex-col items-center px-4 py-1 hover:text-orange-400 relative rounded-full"
       >
         {pathname === "/notifications" ? (
           <svg
@@ -90,7 +90,7 @@ export function Navbar() {
       </Link>
       <Link
         href={`/profile/${currentUser?.username}`}
-        className="flex flex-col items-center px-4 py-1 hover:text-orange-400 rounded-full active:scale-90 transition"
+        className="flex flex-col items-center px-4 py-1 hover:text-orange-400 rounded-full"
       >
         {pathname.startsWith(`/profile/${currentUser?.username}`) ? (
           <svg
