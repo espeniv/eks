9af4cdf6f353281900185161c480df71c29f73a4
Espeniv <espeiv@stud.ntnu.no>
2026-02-19 21:34:04 +0100

    Changed db hosting to appwrite

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -21,8 +21,10 @@ import { Client, Account, Databases, Storage } from "appwrite";
 
 const client = new Client();
 client
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+  .setEndpoint(
+    process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "https://cloud.appwrite.io/v1",
+  )
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "placeholder");
 
 export const account = new Account(client);
 export const databases = new Databases(client);
